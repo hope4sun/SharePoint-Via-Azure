2b997be2ce4b6cfbf4110bb37addd49e6a659b6b
Andrew Carter <a.p.carter@usa.net>
2012-05-07 22:49:15 +0100

    Intial Code

---
+++ AzureSBR/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+using System.ServiceModel.Routing;
+
+namespace AzureSBR.Router
+{
+    public class Program
+    {
+        // Host the service within the console application.
+        public static void Main()
+        {
+            // Create a ServiceHost for the RoutingService.
+            using (ServiceHost serviceHost =
+                new ServiceHost(typeof(RoutingService)))
+            {
+                // Open the ServiceHost to create listeners         
+                // and start listening for messages.
+                Console.WriteLine("The Routing Service configured, opening....");
+                serviceHost.Open();
+                Console.WriteLine("The Routing Service is now running.");
+                Console.WriteLine("Press <ENTER> to terminate router.");
+
+                // The service can now be accessed.
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ AzureSpClient/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.Xml.Linq;
+
+namespace AzureSpClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** SharePoint Cloud Client ****");
+            Console.WriteLine("Press <Enter> to run client.");
+            Console.ReadLine();
+            Console.WriteLine("Starting.");
+
+            // Open a channel to the Routing Service via Azure
+            ChannelFactory<IMyContract> channelFactory = new ChannelFactory<IMyContract>("webRelay");
+            IMyContract channel = channelFactory.CreateChannel();
+
+            // Call the service
+            var response = channel.GetList("Azure Data");
+
+            // Output the returned raw XML
+            Console.WriteLine(response);
+
+            channelFactory.Close();
+            Console.ReadKey();
+        }
+    }
+
+    // Basic inteface for the GetList function of the SharePoint Lists.asmx
+    [ServiceContract(Namespace = "http://schemas.microsoft.com/sharepoint/soap/")]
+    public interface IMyContract
+    {
+        [OperationContract(Action = "http://schemas.microsoft.com/sharepoint/soap/GetList", ReplyAction = "http://schemas.microsoft.com/sharepoint/soap/GetListResponse")]
+        XElement GetList(string listName);
+    }
+}
